6e0f06073f5d22bd95b841cbbe28763039d12096
Ys2025 <1829749816@qq.com>
2020-03-08 11:29:16 +0800

    腾讯AI开放平台-智能闲聊

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         while (true){
             System.out.println("请输入问题：");
-            NlpChat chat = new NlpChat("","qb4lrSlUvaAtInFI","https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat");
+            NlpChat chat = new NlpChat("Appkey","AppID","https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat");
             Map<String,String> map = chat.getAnswer(in.nextLine());
             System.out.println(map.get("ret"));
             System.out.println(map.get("msg"));
